2ba5cee1cbb0eb16dad50532f5a60b07d5caf84c
Aditya Prakash Gupta <47426304+aditya-prakash-gupta@users.noreply.github.com>
2019-10-03 22:28:31 +0530

    Create matrix.cpp

---
+++ matrix.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+	ll n,m;
+	cout<<"Enter dimensions of the two matrices to add\n";
+	cin>>n>>m;  
+	ll a[n][m],b[n][m];
+	cout<<"enter first matrix:\n";     //Input matrix1
+	for(ll i=0;i<n;i++)
+	{
+		for(ll j=0;j<m;j++)
+		cin>>a[i][j];
+	}
+	cout<<"enter second matrix:\n";  //input matrix 2
+	for(ll i=0;i<n;i++)
+	{
+		for(ll j=0;j<m;j++)
+		cin>>b[i][j];
+	}
+	for(ll i=0;i<n;i++)   //add both matrix and store in matrix 1
+	{
+		for(ll j=0;j<m;j++)
+		a[i][j]+=b[i][j];
+	}
+	cout<<"sum:\n";
+	for(ll i=0;i<n;i++)   //printing matrices
+	{
+		for(ll j=0;j<m;j++)
+		cout<<a[i][j]<<" ";
+		cout<<"\n";
+	}
+}
